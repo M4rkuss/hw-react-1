ef8e94974b77062bd7077614ae333985f049efed
m4rkuss <markbu07@gmail.com>
2022-09-29 21:14:40 +0300

    Full 3 tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function HelloReact () {
+  return (
+    React.createElement('h1', null, 'Hello React!')
+  );
+}
+
+function SecondTask () {
+  let userInfo = {
+    name: "Mark",
+    surname: "Buchatskiy",
+    phone: "+38 (095) 623 03 28",
+    email: "markbu07@gmail.com",
+    compareAllInfo: function () {
+      return (
+        `Имя Фамилия: ${this.name} ${this.surname} | Телефон: ${this.phone} | Имейл: ${this.email}`
+      )
+    }
+  }
+  return (
+    <>
+      <h1>Second Task:</h1>
+      <p>{userInfo.compareAllInfo()}</p>
+    </>
+  )
+}
+
+function ThirdTask () {
+  let info = {
+    city: 'Kyiv',
+    country: 'Ukraine',
+    foundation: 930
+  }
+  return (
+    <>
+      <h1>Third Task:</h1>
+      <ul style={{padding: 0}}>
+        <li>{info.city}</li>
+        <li>{info.country}</li>
+        <li>{info.foundation}</li>
+      </ul>
+      <img src="https://eimg.pravda.com/images/doc/1/e/1e4151f-----.jpg" alt="" width={400}/>
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>First task:</h1>
+      <HelloReact/>
+      <br/>
+      <SecondTask/>
+      <ThirdTask/>
     </div>
   );
 }
